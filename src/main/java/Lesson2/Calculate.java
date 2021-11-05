e4578031fbd5620c283dfedbe1acdecd657cdc25
Vega <vega1982ivan@gmail.com>
2021-11-05 22:11:42 +0700

    Simple Calculation + Teorema cosinusov

--- src/main/java/Lesson2/Calculate.java
+++ src/main/java/Lesson2/Calculate.java
@@ -1,23 +1,29 @@
 package main.java.Lesson2;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sqrt;
+
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Рассчёты");
 
-        long summa = summ(35, 7);
+        long summa = sum(35, 7);
         System.out.println("Сумма чисел 35 и 7 = " + summa);
 
         long subtraction = minus(35, 7);
-        System.out.println("Разность чисел 35 и 7 = " +subtraction);
+        System.out.println("Разность чисел 35 и 7 = " + subtraction);
 
         long multi = multiplication(35, 7);
-        System.out.println("Умножение чисел 35 и 7 = " +multi);
+        System.out.println("Умножение чисел 35 и 7 = " + multi);
 
         double div = division(35, 7);
-        System.out.println("Деление чисел 35 и 7 = " +div);
+        System.out.println("Деление чисел 35 и 7 = " + div);
+
+        double teoremaCosinusov = cosinusTeorema(25, 10, 33);
+        System.out.println("По теореме косинусов, при b=25; c=10; углу 33, длина стороны a = " + teoremaCosinusov);
 
     }
-    public static long summ(long arg1, long arg2) {
+    public static long sum(long arg1, long arg2) {
         //Расчёт суммы двух чисел
         long result = arg1 + arg2;
         return result;
@@ -37,4 +43,14 @@ public class Calculate {
         double result = arg1 / arg2;
         return result;
     }
+    public static double cosinusTeorema(double b, double c, double ugol) {
+        /* Теорема синусов. a^2 = b^2 + c^2 - 2bc*cos угла альфа.
+        Для нахождения длины стороны "a", из результата расчёта
+        по формуле необходимо извлечь квадратный корень */
+        double cosinusUgla = cos(ugol);
+        double subResult = (b * b) + (c * c) - ((2 * b * c) * cosinusUgla);
+        double result = sqrt(subResult);
+        return result;
+    }
+
 }
