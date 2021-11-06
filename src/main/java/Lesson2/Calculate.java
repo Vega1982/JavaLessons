04b2a2f443c87f0662e71f1b13668554f83ee6a4
Vega1982 <vega1982ivan@gmail.com>
2021-11-06 17:33:45 +0700

    Added JavaDoc. Renamed some methods and variables.

--- src/main/java/Lesson2/Calculate.java
+++ src/main/java/Lesson2/Calculate.java
@@ -19,36 +19,63 @@ public class Calculate {
         double div = division(35, 7);
         System.out.println("Деление чисел 35 и 7 = " + div);
 
-        double teoremaCosinusov = cosinusTeorema(25, 10, 33);
-        System.out.println("По теореме косинусов, при b=25; c=10; углу 33, длина стороны a = " + teoremaCosinusov);
+        double theorCos = cosineTheorem(25, 10, 33);
+        System.out.println("По теореме косинусов, при b=25; c=10; углу 33, длина стороны a = " + theorCos);
 
     }
+
+    /**
+     * @param arg1 слагаемое
+     * @param arg2 слагаемое
+     * @return сумма
+     */
     public static long sum(long arg1, long arg2) {
-        //Расчёт суммы двух чисел
         long result = arg1 + arg2;
         return result;
     }
+
+    /**
+     * @param arg1 уменьшаемое
+     * @param arg2 вычитаемое
+     * @return разность
+     */
     public static long minus(long arg1, long arg2) {
-        //Расчёт разности двух чисел
         long result = arg1 - arg2;
         return result;
     }
+
+    /**
+     * @param arg1 умножаемое
+     * @param arg2 умножаемое
+     * @return произведение
+     */
     public static long multiplication(long arg1, long arg2) {
-        //Расчёт умножения двух чисел
         long result = arg1 * arg2;
         return result;
     }
+
+    /**
+     * @param arg1 делимое
+     * @param arg2 делитель
+     * @return частное
+     */
     public static double division(double arg1, double arg2) {
-        //Расчёт деления двух чисел
         double result = arg1 / arg2;
         return result;
     }
-    public static double cosinusTeorema(double b, double c, double ugol) {
-        /* Теорема косинусов. a^2 = b^2 + c^2 - 2bc*cos угла альфа.
-        Для нахождения длины стороны "a", из результата расчёта
-        по формуле необходимо извлечь квадратный корень */
-        double cosinusUgla = cos(ugol);
-        double subResult = (b * b) + (c * c) - ((2 * b * c) * cosinusUgla);
+
+    /**
+     * Теорема косинусов для треугольников. a^2 = b^2 + c^2 - 2bc*cos угла альфа.
+     * Для нахождения длины стороны "a", из результата расчёта
+     * по формуле необходимо извлечь квадратный корень.
+     * @param b длина стороны b
+     * @param c длина стороны c
+     * @param angle величина угла альфа в градусах
+     * @return длина стороны а
+     */
+    public static double cosineTheorem(double b, double c, double angle) {
+        double cosineAngle = cos(angle);
+        double subResult = (b * b) + (c * c) - ((2 * b * c) * cosineAngle);
         double result = sqrt(subResult);
         return result;
     }
