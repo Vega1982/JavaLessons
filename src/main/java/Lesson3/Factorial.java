9d43b8297e8606d692ef659fc716f1f32a0aca35
Vega <vega1982ivan@gmail.com>
2021-11-09 00:18:07 +0700

    Factorial

---
+++ src/main/java/Lesson3/Factorial.java
@@ -0,0 +1,11 @@
+package main.java.Lesson3;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите число n для рассчёта его факториала: ");
+        long factorialLenght = console.nextInt();
+    }
+}
