f040a430ad4cfd829543bf6c337c6ba02055270a
Vega <vega1982ivan@gmail.com>
2021-11-11 22:51:31 +0700

    Square Root

---
+++ src/main/java/Lesson4/SquareRoot.java
@@ -0,0 +1,16 @@
+package main.java.Lesson4;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите целое положительное число n для расчёта его квадратного корня: ");
+
+        // задаём начальную переменную
+        // получаем от пользователя число, для которого будем вычислять кв. корень
+        int nForRoot = console.nextInt();
+
+
+    }
+}
