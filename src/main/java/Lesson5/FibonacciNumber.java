53e3876ebc32a10e0a7b2047ed6d3882110c1019
Vega <vega1982ivan@gmail.com>
2021-11-15 20:14:59 +0700

    Fibonacci

--- src/main/java/Lesson5/FibonacciNumber.java
+++ src/main/java/Lesson5/FibonacciNumber.java
@@ -1,7 +1,55 @@
 package main.java.Lesson5;
 
+import java.util.Scanner;
+
 public class FibonacciNumber {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите целое положительное число индекса n");
+        System.out.print("для расчёта значения его числа Фибоначчи: ");
+        int n = console.nextInt();
+        int fN = 0;
+
+        //n - индекс на входе
+        //fN - искомое число Фибоначчи для индекса n
+        //slipperyN - скользящий индекс n для промежуточных расчётов
+        //fN1 - число Фибоначчи для индекса минус 1 от текущего slipperyN
+        //fN2 - число Фибоначчи для индекса минус 2 от текущего slipperyN
+
+        //Программа считает только начиная с n >= 4, поэтому для n == 1...3 значения выдаются без расчётов:
+        if (n == 1) {
+            fN = 0;
+            System.out.println("Число Фибоначчи для n" + n + " равно " + fN);
+            System.exit(0);
+        }
+
+        if (n == 2) {
+            fN = 1;
+            System.out.println("Число Фибоначчи для n" + n + " равно " + fN);
+            System.exit(0);
+        }
+
+        if (n == 3) {
+            fN = 1;
+            System.out.println("Число Фибоначчи для n" + n + " равно " + fN);
+            System.exit(0);
+        }
+
+        //Основной цикл расчётов
+        for (int fN2 = 0, fN1 = 1, slipperyN = 3; slipperyN <= n; slipperyN++) {
+            fN = fN1 + fN2;
+            fN2 = fN1;
+            fN1 = fN;
+
+            //Проверка для выхода из цикла и программы
+            if (slipperyN == n) {
+                System.out.println("Число Фибоначчи для n" + n + " равно " + fN);
+                System.exit(0);
+            }
+
+        }
+
 
     }
 }
+
