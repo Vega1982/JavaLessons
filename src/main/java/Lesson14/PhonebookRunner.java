43293e79a4a265ea683d7b4d10dfaeb585156f1b
Vega <vega1982ivan@gmail.com>
2021-12-19 19:08:01 +0700

    Phone book. 2nd step.

--- src/main/java/Lesson14/PhonebookRunner.java
+++ src/main/java/Lesson14/PhonebookRunner.java
@@ -1,9 +1,6 @@
 package main.java.Lesson14;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Random;
+import java.util.*;
 
 public class PhonebookRunner {
     public static void main(String[] args) {
@@ -27,12 +24,49 @@ public class PhonebookRunner {
             mapOfSubscriber.put(numbersForMap(k), subscriberForMap(k, listOfSubscribers));
         }
 
-        // после того, как список абонентов наполнен, я хочу заполнить для каждого ArrayList boundedSubscribers,
-        // поместив туда 100 ссылок на других рандомных абонентов основгого списка listOfSubscribers
-        // но как это сделать?
+        // После того, как список абонентов наполнен, добавляем каждому абоненту 100 других как связанных с ним
+        // Код ниже любезно предоставлен преподаваетелем
+        Random random = new Random();
+        for (Subscriber listOfSubscriber : listOfSubscribers) {
+            for (int i = 0; i < 100; i++) {
+                Subscriber randomSubscriber;
+                randomSubscriber = listOfSubscribers.get(random.nextInt(listOfSubscribers.size()));
+                listOfSubscriber.getBoundedSubscribers().add(randomSubscriber);
+            }
+        }
+
+        // Найдём самый часто встречающийся номер из числа связанных
+        // Создадим список для хранения тел. номеров
+        Long[] array = new Long[10000000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Long.valueOf(i + 100);
+        }
+
+        // Создадим коллекцию Set для временного хранения сотни номеров (они тоже в Set)
+        HashSet<Subscriber> setForNumber = new HashSet<>();
+
+        // Цикл проходит по всем абонентам и извлекает колекцию связанных абонентов
+        for (int i = 0, j = 100, k = 0; i < subscribersAmount; i++) {
+            // Сущьность для временного хранения одного абонента
+            Subscriber tempSubscriber;
+            tempSubscriber = listOfSubscribers.get(i);
+            // наполняем временную коллекцию сотней абонентов
+            setForNumber.addAll(tempSubscriber.getBoundedSubscribers());
+            // втроенный цикл для пробега по колллекции из сотни связанных абонентов
+            for (Subscriber tempS : setForNumber) {
+                Long t = tempS.getTelNumber();
+                // каждый из сотни номеров добавляем в массив
+                array[j + k] = t;
+                k++;
+            }
+            j = j + 100;
+            k = 0;
+        }
+        System.out.println("Самый часто повторяющийся номер в связанных номерах: " + mostPopularPhoneNumber(array));
 
 
     }
+
 
     // взятый из инета метод генерации случайных строк. Здесь они формируют ФИО сотрудника
     public static String getRandomFIO() {
@@ -69,6 +103,48 @@ public class PhonebookRunner {
     // Метод, возвращающий объект абонента из списка (по-порядку)
     public static Subscriber subscriberForMap(int k, ArrayList<Subscriber> list) {
         return list.get(k);
+    }
+
+//    // Метод поиска самого часто встречающегося связанного тел. номера
+//    public static long boundTelNum() {
+//
+//        long bN = 100;
+//        return bN;
+//    }
+
+    // взятый из инета метод для поиска самого часто встречающегося элемента в массиве
+    public static Long mostPopularPhoneNumber(Long[] array) {
+
+        long[] counts = new long[100000];
+        int maxCount = 0;
+        int maxKey = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            try {
+                counts[Math.toIntExact(array[i])]++;
+            } catch (Exception e) {
+                if (maxKey == 100) {
+                    break;
+                }
+            }
+
+            try {
+                if (counts[Math.toIntExact(array[i])] > maxCount) {
+                    maxCount = Math.toIntExact(counts[Math.toIntExact(array[i])]);
+                    maxKey = Math.toIntExact(array[i]);
+                }
+            } catch (Exception e) {
+                if (maxKey == 100) {
+                    break;
+                }
+
+            }
+            if (maxKey == 100) {
+                break;
+            }
+
+        }
+        return Long.valueOf(maxKey);
     }
 
 }
--- src/main/java/Lesson14/Subscriber.java
+++ src/main/java/Lesson14/Subscriber.java
@@ -1,16 +1,18 @@
 package main.java.Lesson14;
 
-import java.util.ArrayList;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Subscriber {
     private final String FIO;
     private final long telNumber;
-    private ArrayList<Long> boundedSubscribers = new ArrayList<Long>();
+    // Код ниже любезно предоставлен преподаваетелем
+    private final Set<Subscriber> boundedSubscribers = new HashSet<>();
 
     public Subscriber(String FIO, Long telNumber) {
         this.FIO = FIO;
         this.telNumber = telNumber;
-        this.boundedSubscribers = boundedSubscribers;
     }
 
     public String getFIO() {
@@ -21,11 +23,33 @@ public class Subscriber {
         return telNumber;
     }
 
-    public ArrayList<Long> getBoundedSubscribers() {
+    // Код ниже любезно предоставлен преподаваетелем
+    public Set<Subscriber> getBoundedSubscribers() {
         return boundedSubscribers;
     }
 
-    public void setBoundedSubscribers(ArrayList<Long> boundedSubscribers) {
-        this.boundedSubscribers = boundedSubscribers;
+    public Set<Subscriber> getBoundedSubscribersByNumber() {
+        return boundedSubscribers;
+    }
+
+
+    // Код ниже любезно предоставлен преподаваетелем
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subscriber that = (Subscriber) o;
+
+        if (telNumber != that.telNumber) return false;
+        return FIO != null ? FIO.equals(that.FIO) : that.FIO == null;
+    }
+
+    // Код ниже любезно предоставлен преподаваетелем
+    @Override
+    public int hashCode() {
+        int result = FIO != null ? FIO.hashCode() : 0;
+        result = 31 * result + (int) (telNumber ^ (telNumber >>> 32));
+        return result;
     }
 }
